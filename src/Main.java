9853d9a5aa9ee4fe5421bcae22c601cb1479ca82
wasamilk <vin5004@naver.com>
2024-01-05 13:51:47 +0900

    [24.01.05] 프로젝트 init 내용 변경

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import TestPkg.TestPrint;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world main~~!");
+
+        TestPrint print = new TestPrint();
+        print.testPrint();
+    }
+}
---
+++ src/TestPkg/TestPrint.java
@@ -0,0 +1,32 @@
+package TestPkg;
+
+import java.util.Scanner;
+
+public class TestPrint {
+
+    public void testPrint() {
+        /* 출력 테스트 */
+        System.out.println(" --- 출력 테스트 --- ");
+
+        int num1 = 10;
+        System.out.printf("num1 : %d\n", num1);
+        System.out.printf("num1 : %d%n", num1); // OS마다 줄바꿈이 다를 수 있으므로 %n 사용이 안전
+        System.out.println("num1 : " + num1);
+
+        System.out.println("num1 : " + num1);
+        System.out.printf("num1 : %d", num1);
+
+        /* 입력 테스트 */
+        // 최신 방법은 Console
+        System.out.println("\n\n --- 입력 테스트 --- ");
+        System.out.println("숫자 입력 : ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        System.out.println("입력한 숫자(String) : " + input);
+        System.out.println("입력한 숫자(int) : " + Integer.parseInt(input));
+        System.out.println("입력한 숫자(float) : " + Float.parseFloat(input));
+    }
+
+
+}
