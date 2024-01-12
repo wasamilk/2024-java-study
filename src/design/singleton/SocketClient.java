0713c43b49edbbe779562fcc135fca6d888ec5b3
wasamilk <vin5004@naver.com>
2024-01-13 00:56:01 +0900

    [24.01.13] 디자인패턴 - singleton pattern

---
+++ src/design/singleton/SocketClient.java
@@ -0,0 +1,20 @@
+package design.singleton;
+
+public class SocketClient {
+    private static SocketClient socketClient = null;
+
+    private SocketClient(){
+
+    }
+
+    public static SocketClient getInstance(){
+        if(socketClient == null){
+            socketClient = new SocketClient();
+        }
+        return socketClient;
+    }
+
+    public void connect(){
+        System.out.println("connect");
+    }
+}
